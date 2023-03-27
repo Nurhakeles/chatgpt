126b95736a106063e7b4fff6dab7d6e2e827659c
Nurhakeles <113163120+Nurhakeles@users.noreply.github.com>
2023-03-27 14:34:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './normal.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        
+      </section>
+    </div>
+  );
+}
+
+export default App;
